0f325f7e2c596f485f59335d1c31f99d4551a575
PLABAN NAYAK <plaban1981@users.noreply.github.com>
2022-10-10 18:54:52 +0530

    Update question6.py

--- homework_5/question6.py
+++ homework_5/question6.py
@@ -1,5 +1,34 @@
-import requests
-url = "http://localhost:9696/predict"
-client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
-response = requests.post(url, json=client).json()
-print(response)
+from flask import Flask
+from flask import request
+from flask import jsonify
+import joblib
+
+import pickle
+
+def load(filename):
+        return joblib.load(filename)
+
+
+dv = load('dv.bin')
+model = load('model1.bin')
+
+app = Flask('churn_predictions')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    cc_data = request.get_json()
+
+    X = dv.transform([cc_data])
+    y_pred = model.predict_proba(X)[0, 1]
+    churn = y_pred >= 0.5
+
+    result = {
+        'churn_probability': float(y_pred),
+        'churn': bool(churn)
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
